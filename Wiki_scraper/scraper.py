b59b35f4b3b31397a4eed9a28d255d804dee3309
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-04-18 09:00:26 +0530

    Modified scraper to store the words in a csv file rather than a text file

--- Wiki_scraper/scraper.py
+++ Wiki_scraper/scraper.py
@@ -1,22 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-'''i=0
-arr={}'''
 headers={ 'User-Agent': 'Wikiscraper 1.0'
 		}
-f=open("../Wordlist.txt", 'w+')
+f=open("../Wordlist.csv", 'w+')
 source_code=requests.get("https://en.wiktionary.org/wiki/Appendix:1000_basic_English_words", headers=headers)
 text=source_code.text
 soup=BeautifulSoup(text)
 for link in soup.findAll('div', {'class':'mw-content-ltr'}):
 	soup1=BeautifulSoup(str(link))
-	'''for link1 in soup1.findAll('b'):
-		arr[i]=link1.string
-		i=i+1'''
 	for link2 in soup1.findAll('dl'):
 		soup2=BeautifulSoup(str(link2))
 		for link3 in soup2.findAll('a'):
-			f.write(link3.string+"\n")
+			f.write(link3.string+",")
 		f.write("\n")
 f.close()
